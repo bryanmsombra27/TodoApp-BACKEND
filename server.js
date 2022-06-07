46fe90ab592a4ad49ada23ee5399ef712b0507f3
bryanmsombra27 <bryanjm96@gmail.com>
2022-06-06 22:58:36 -0500

    inicio del proyecto

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { conexionBD } = require('./config/db');
+
 const port  = 3000;
 
 
@@ -10,15 +10,8 @@ app.use(express.json());
 app.use(cors());
 
 
-let tareas =[
-    {id:Math.round(Math.random()*100 * new Date().getSeconds() +  new Date().getMilliseconds()),nombre:'Aprender React',estado:false},
-    {id:Math.round(Math.random()*100 * new Date().getSeconds() +  new Date().getMilliseconds()),nombre:'Aprender Tailwind CSS',estado:false},
-    {id:Math.round(Math.random()*100 * new Date().getSeconds() +  new Date().getMilliseconds()),nombre:'Aprender Bootstrap',estado:false},
-];
-conexionBD();
-
 
     app.listen(port, () => {
-        console.log('escuchando peticiones por el puerto 3000');
+        console.log('escuchando peticiones por el puerto: ' + port);
     });
     
